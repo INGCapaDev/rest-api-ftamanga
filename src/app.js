280464f89d42a146ccc1216f4f17d41289b742e2
Alvaro Capaceta <ingcapadev@gmail.com>
2023-03-21 23:42:49 -0700

    Creacion carpeta public

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import './utils/handleEnv.js';
+
+const app = express();
+
+// TODO App Settings
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
