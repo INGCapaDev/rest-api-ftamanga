79a96a22f8ea38af177199fe5b785a08f33cb5ed
Alvaro Capaceta <ingcapadev@gmail.com>
2023-03-21 23:57:16 -0700

    Configuracion de conexion a base de datos

---
+++ src/database/config.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize';
+
+const database = process.env.MYSQL_DATABASE;
+const user = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const host = process.env.MYSQL_HOST;
+
+export const sequelize = new Sequelize(database, user, password, {
+  host,
+  dialect: 'mysql',
+});
+
+export const dbConnectMySql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('**** SUCCESS_MYSQL_CONNECTION ****');
+  } catch (error) {
+    console.log('**** ERROR_MYSQL_CONNECTION ****');
+  }
+};
