3d6588ad2ac093d75b26a22c63c3a33c113b9bc1
Alvaro Capaceta Magallanes <2020030170@upsin.edu.mx>
2023-03-22 11:38:02 -0700

    Modelo de productos

---
+++ src/models/products.model.js
@@ -0,0 +1,10 @@
+import { DataTypes, Transaction } from 'sequelize';
+import { sequelize } from '../database/config.js';
+
+export const productsModel = sequelize.define('products', {
+  name: { type: DataTypes.STRING, allowNull: false },
+  price: { type: DataTypes.FLOAT, allowNull: false },
+  editorial: { type: DataTypes.STRING, allowNull: false },
+  img: { type: DataTypes.STRING },
+  year: { type: DataTypes.NUMBER, allowNull: false },
+});
