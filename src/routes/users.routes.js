6fa4026e6fd9163882f2989c704b76374c6943f9
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-10 14:57:01 -0600

    Router Usuarios getUsers, getUserDetail, registerUser, updateUser, deleteUser

---
+++ src/routes/users.routes.js
@@ -0,0 +1,36 @@
+import express from 'express';
+const userRoutes = express.Router();
+import usersController from '../controllers/users.controller.js';
+
+// TODO Middlewares and Validators
+import usersValidator from '../validators/users.validator.js';
+
+/**
+ * * Get All Users
+ */
+userRoutes.get('/', usersController.getUsers);
+
+/**
+ * * Get User Detail
+ */
+userRoutes.get('/:id', usersValidator.isValidId, usersController.getUserDetail);
+
+/**
+ * * Create New User
+ */
+userRoutes.post('/', usersValidator.isValidUser, usersController.registerUser);
+
+/**
+ * * Update User
+ */
+userRoutes.put(
+  '/:id',
+  usersValidator.isValidId,
+  usersValidator.isValidUser,
+  usersController.updateUser
+);
+
+/**
+ * ! Delete User
+ */
+userRoutes.delete('/:id', usersValidator.isValidId, usersController.deleteUser);
