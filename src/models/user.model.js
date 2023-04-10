e571257ca6689c89d7644da5332d5b89def6a18a
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-10 13:11:56 -0600

    Modelo de usuarios

---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+import { sequelize } from '../database/config.js';
+import { DataTypes } from 'sequelize';
+
+// TODO SQL Table users
+export const usersModel = sequelize.define(
+  'users',
+  {
+    name: { type: DataTypes.STRING, allowNull: false },
+    user: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    adress: { type: DataTypes.STRING },
+    phone: { type: DataTypes.STRING },
+    email: { type: DataTypes, allowNull: false, unique: true },
+    role: { type: DataTypes.ENUM(['User', 'Admin']) },
+  },
+  {
+    timestamps: true, // TODO createdAt, updatedAt
+  }
+);
