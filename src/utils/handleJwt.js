209541b1e52a86a626e1a7e1177dfbd0365e5d64
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-10 14:10:16 -0600

    Utils Manejador de JsonWebTokens

--- src/utils/handleJwt.js
+++ src/utils/handleJwt.js
@@ -1,1 +1,28 @@
 import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+/**
+ * * user = The user object
+ * @param {*} user
+ * @returns
+ */
+export const tokenSign = (user) => {
+  try {
+    const sign = jwt.sign({ id: user.id, role: user.role }, JWT_SECRET, {
+      expiresIn: '2h',
+    });
+
+    return sign;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const verifyToken = (tokenJwt) => {
+  try {
+    return jwt.verifyToken(tokenJwt, JWT_SECRET);
+  } catch (error) {
+    return null;
+  }
+};
