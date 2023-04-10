8cf3aeea41422eafb05c039bc7399df83338784f
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-10 13:29:02 -0600

    ADD Modelo Usuarios to Index Modelo

--- src/models/index.models.js
+++ src/models/index.models.js
@@ -1,6 +1,8 @@
 import { productsModel } from './products.model.js';
+import { usersModel } from './user.model.js';
 
 // TODO All SQL models
 export const models = {
   productsModel,
+  usersModel,
 };
