16baf086189f8c45818a4bb03e908f559e8085a3
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-13 10:08:36 -0700

    Controlador Ventas createSale, getSales, searchSale

---
+++ src/controllers/sales.controller.js
@@ -0,0 +1,73 @@
+import { matchedData } from 'express-validator';
+import { models } from '../models/index.models.js';
+
+// TODO Error Handler
+import { handleHttpError } from '../utils/handleError.js';
+
+/**
+ * * Get All Sales
+ * @param {*} req
+ * @param {*} res
+ */
+const getSales = async (req, res) => {
+  try {
+    const data = await models.salesModel.findAll({
+      include: [
+        {
+          model: models.usersModel,
+        },
+        {
+          model: models.salesDetailsModel,
+        },
+      ],
+    });
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, 'ERROR_GET_SALES');
+  }
+};
+
+/**
+ * * Search Sale
+ * @param {*} req
+ * @param {*} res
+ */
+const searchSale = async (req, res) => {
+  try {
+    const { id } = matchedData(req);
+    const data = await models.salesModel.findOne({
+      include: [
+        {
+          model: models.usersModel,
+        },
+        {
+          model: models.salesDetailsModel,
+        },
+      ],
+      where: { id: id },
+    });
+    if (!data) {
+      return handleHttpError(res, 'ERROR_SALE_NOT_EXISTS');
+    }
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, 'ERROR_GET_PRODUCT_DETAIL');
+  }
+};
+
+/**
+ * * Create New Sale
+ * @param {*} req
+ * @param {*} res
+ */
+const createSale = async (req, res) => {
+  try {
+    const sale = matchedData(req);
+    const data = await models.productsModel.create(sale);
+    res.json(data);
+  } catch (error) {
+    handleHttpError(res, 'ERROR_CREATE_SALE');
+  }
+};
+
+export default { createSale, getSales, searchSale };
--- src/models/index.models.js
+++ src/models/index.models.js
@@ -3,6 +3,10 @@ import { usersModel } from './user.model.js';
 import { salesModel } from './sales.model.js';
 import { salesDetailsModel } from './sales_details.model.js';
 
+salesModel.hasOne(usersModel, { foreignKey: 'id_user' });
+salesModel.hasMany(salesDetailsModel, { foreignKey: 'id_sale' });
+salesDetailsModel.hasOne(productsModel, { foreignKey: 'id_product' });
+
 // TODO All SQL models
 export const models = {
   productsModel,
