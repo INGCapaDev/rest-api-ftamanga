8bfaa0f4391c69148adc97cf9f37973673e3cf9e
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-13 09:31:20 -0700

    Modelo de Detalles_Ventas

---
+++ src/models/sales_details.model.js
@@ -0,0 +1,16 @@
+import { sequelize } from '../database/config.js';
+import { DataTypes } from 'sequelize';
+
+// TODO SQL Table users
+export const salesDetailsModel = sequelize.define(
+  'sales_details',
+  {
+    id_sale: { type: DataTypes.INTENGER, allowNull: false },
+    id_product: { type: DataTypes.INTENGER, allowNull: false },
+    quantity: { type: DataTypes.INTENGER, allowNull: false },
+    subtotal: { type: DataTypes.DOUBLE, allowNull: false, unique: true },
+  },
+  {
+    timestamps: true, // TODO createdAt, updatedAt
+  }
+);
